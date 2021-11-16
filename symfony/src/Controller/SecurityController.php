b0a0fcc2e24c9327d5cd2bd897353344a5f26dc6
Diamondik <grishabatov83@gmail.com>
2021-11-16 20:47:30 +0300

    init

---
+++ symfony/src/Controller/SecurityController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login", methods={"POST", "GET"})
+     */
+    public function login(Request $request): Response
+    {
+        return $this->json([
+                'user' => $this->getUser() ? $this->getUser()->getId() : null]
+        );
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+}
