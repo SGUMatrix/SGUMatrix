9f4ef370d06e7cd888343c5f356a8600c2f503fd
Diamondik <grishabatov83@gmail.com>
2021-11-19 01:48:39 +0300

    Инвайтер контроллер

--- symfony/src/Controller/InviterController.php
+++ symfony/src/Controller/InviterController.php
@@ -5,26 +5,37 @@ use App\Entity\UserProfile;
 use App\Repository\UserProfileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-class inviterController extends AbstractController
+
+class InviterController extends AbstractController
 {
+    /**
+     * @var UserProfileRepository
+     */
     private UserProfileRepository $userProfileRepository;
 
+    /**
+     * @param UserProfileRepository $userProfileRepository
+     */
     public function __construct(UserProfileRepository $userProfileRepository)
     {
         $this->userProfileRepository = $userProfileRepository;
     }
+
     #[Route('/inviter', name: 'app_inviter')]
     public function index(Request $request): JsonResponse
     {
-        $data = json_decode($request->getContent(), true);
-        $referral = $this->userProfileRepository->findOneByUsername($data['referral']);
-        return new JsonResponse([
-            'firstName' => $referral->getReferral()?->getFirstName(),
-            'lastName' => $referral->getReferral()?->getLastName(),
+        $username = $request->query->get('username');
+        $referral = $this->userProfileRepository->findOneByUsername($username);
 
+        if (!$referral) {
+            return new JsonResponse(['success' => false, 'message' => 'Наставник с таким логином не существует']);
+        }
+
+        return new JsonResponse([
+            'firstName' => $referral->getFirstName(),
+            'lastName' => $referral->getLastName(),
         ]);
     }
 }
